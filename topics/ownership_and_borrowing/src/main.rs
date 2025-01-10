864edf6e0c490559379e4c87890fe8699579883a
Soulsniper <soulsniper155@gmail.com>
2025-01-10 16:18:14 +0500

    Ownership and borrowing

---
+++ topics/ownership_and_borrowing/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // Ownership
+    // exercise 1
+    // Use as many approaches as you can to make it work
+    let x = String::from("Hello world");
+    let y = x.clone();
+    println!("{}, {}", x, y);
+}
