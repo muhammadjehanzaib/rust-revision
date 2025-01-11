690db21c13061824271bf65d21db4026121fd660
Soulsniper <soulsniper155@gmail.com>
2025-01-11 22:16:52 +0500

    Exercise 2 solved

--- topics/ownership_and_borrowing/src/main.rs
+++ topics/ownership_and_borrowing/src/main.rs
@@ -2,7 +2,20 @@ fn main() {
     // Ownership
     // exercise 1
     // Use as many approaches as you can to make it work
-    let x = String::from("Hello world");
-    let y = x.clone();
-    println!("{}, {}", x, y);
+    // let x = String::from("Hello world");
+    // let y = x.clone();
+    // println!("{}, {}", x, y);
+
+    // exercise 2
+    let s1 = String::from("hello world!");
+    let s2 = take_ownership(s1);
+    println!("{}", s2);
+
+    //exercise 3
+}
+
+#[allow(dead_code)]
+fn take_ownership(s: String) -> String {
+    println!("{}", s);
+    s
 }
