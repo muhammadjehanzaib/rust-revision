3f0c5643ae15d0a3680172e68eb5d8bf48441db6
muhammadjehanzaib <soulsniper155@gmail.com>
2025-01-24 08:19:59 +0500

    Methods and Associated functions topic create and initialized

--- topics/associated_functions_/src/main.rs
+++ topics/associated_functions_/src/main.rs
@@ -1,1 +1,19 @@
-fn main() {}
+fn main() {
+    let rect = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    assert_eq!(rect.area(), 1500);
+    println!("Success");
+}
+
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
