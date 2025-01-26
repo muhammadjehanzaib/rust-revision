494dbace2652f0547b042d0cecde5eafa969a6fa
muhammadjehanzaib <soulsniper155@gmail.com>
2025-01-26 06:44:43 +0500

    Exercises 1 and 2 solved

--- topics/const_generics/src/main.rs
+++ topics/const_generics/src/main.rs
@@ -1,3 +1,29 @@
 fn main() {
-    println!("Hello, world!");
+    // Exercise 1
+    // let array = [
+    //     Array{
+    //         data: [1,2,3]
+    //     },
+    //     Array{
+    //         data: [3,4,5]
+    //     },
+    //     Array{
+    //         data: [6,7,8]
+    //     },
+    // ];
+
+    // Exercsie 2
+    let arr = [1, 2, 3];
+    print_array(arr);
+
+    let arr = ["hello", ",world"];
+    print_array(arr);
+}
+
+fn print_array<T: std::fmt::Debug, const N: usize>(arr: [T; N]) {
+    println!("{:?}", arr);
+}
+
+struct Array<T, const N: usize> {
+    data: [T; N],
 }
