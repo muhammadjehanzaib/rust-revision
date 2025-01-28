deb89e100360ce04eab2dcc5cf6d6f6f7e6294c0
muhammadjehanzaib <soulsniper155@gmail.com>
2025-01-28 16:32:49 +0500

    more exercises covered

--- topics/trait_object/src/main.rs
+++ topics/trait_object/src/main.rs
@@ -1,58 +1,94 @@
-
-trait Bird {
-    fn quack(&self) -> String;
-}
-
-struct Duck;
-impl Duck {
-    fn swim(&self) {
-        println!("Look, the duck is swimming")
-    }
-}
-struct Swan;
-impl Swan {
-    fn fly(&self) {
-        println!("Look, the duck.. oh sorry, the swan is flying")
-    }
-}
-
-impl Bird for Duck {
-    fn quack(&self) -> String{
-        "duck duck".to_string()
-    }
-}
-
-impl Bird for Swan {
-    fn quack(&self) -> String{
-        "swan swan".to_string()
-    }
-}
+use std::os::unix::raw::pid_t;
 
 fn main() {
-    // FILL in the blank.
-    let duck = Duck;
-    duck.swim();
+    // Exercsie 1
+    // let duck = Duck;
+    // duck.swim();
+    // let bird = hatch_a_bird(2);
+    // assert_eq!(bird.quack(), "duck duck");
+    // let bird = hatch_a_bird(1);
+    // assert_eq!(bird.quack(), "swan swan");
+    // println!("Success!");
 
-    let bird = hatch_a_bird(2);
-    // This bird has forgotten how to swim, so below line will cause an error.
-    // bird.swim();
-    // But it can quak.
-    assert_eq!(bird.quack(), "duck duck");
+    // Exercise 2
+    // let birds: [Box<dyn Bird>; 2] = [Box::new(Duck {}), Box::new(Swan {})];
 
-    let bird = hatch_a_bird(1);
-    // This bird has forgotten how to fly, so below line will cause an error.
-    // bird.fly();
-    // But it can quak too.
-    assert_eq!(bird.quack(), "swan swan");
+    // for bird in birds {
+    //     println!("{}", bird.quack());
+    // }
 
-    println!("Success!");
-}   
+    // Exercise 3
+    // let x = 1.1f64;
+    // let y= 8u8;
+    
+    // draw_with_box(Box::new(x));
+    // draw_with_ref(&y);
+    // println!("Success");
 
-// IMPLEMENT this function.
-fn hatch_a_bird(species: u8) -> Box<dyn Bird> {
-    match species {
-        1 => Box::new(Swan{}),
-        2 => Box::new(Duck {}),
-        _ => panic!(),
+    // Exercise 4
+    
+}
+
+fn draw_with_box(x: Box<dyn Draw>) {
+    x.draw();
+}
+fn draw_with_ref(y: &dyn Draw) {
+    y.draw();
+}
+
+
+trait Draw {
+    fn draw(&self)-> String;
+}
+
+impl Draw for u8{
+    fn draw(&self) -> String {
+        format!("u8: {}", *self)
     }
 }
+
+impl Draw for f64 {
+    fn draw(&self)-> String {
+        format!("f64: {}", *self)
+    }
+}
+
+
+
+// // IMPLEMENT this function.
+// fn hatch_a_bird(species: u8) -> Box<dyn Bird> {
+//     match species {
+//         1 => Box::new(Swan{}),
+//         2 => Box::new(Duck {}),
+//         _ => panic!(),
+//     }
+//}
+
+// trait Bird {
+//     fn quack(&self) -> String;
+// }
+
+// struct Duck;
+// impl Duck {
+//     fn swim(&self) {
+//         println!("Look, the duck is swimming")
+//     }
+// }
+// struct Swan;
+// impl Swan {
+//     fn fly(&self) {
+//         println!("Look, the duck.. oh sorry, the swan is flying")
+//     }
+// }
+
+// impl Bird for Duck {
+//     fn quack(&self) -> String {
+//         "duck duck".to_string()
+//     }
+// }
+
+// impl Bird for Swan {
+//     fn quack(&self) -> String {
+//         "swan swan".to_string()
+//     }
+// }
