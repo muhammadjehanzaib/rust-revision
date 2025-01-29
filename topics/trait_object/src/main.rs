b3920c89f808abb7352c8a5fb68eaafe9d793f6f
muhammadjehanzaib <soulsniper155@gmail.com>
2025-01-29 07:59:44 +0500

    All Exercises Solved

--- topics/trait_object/src/main.rs
+++ topics/trait_object/src/main.rs
@@ -1,5 +1,3 @@
-use std::os::unix::raw::pid_t;
-
 fn main() {
     // Exercsie 1
     // let duck = Duck;
@@ -20,40 +18,90 @@ fn main() {
     // Exercise 3
     // let x = 1.1f64;
     // let y= 8u8;
-    
+
     // draw_with_box(Box::new(x));
     // draw_with_ref(&y);
     // println!("Success");
 
     // Exercise 4
-    
+    // let x = 10u8;
+    // let y = 1.33f64;
+
+    // static_dispatch(x);
+    // dynamic_dispatch(&y);
+
+    // Exercise 5
+    my_function(Box::new(8u8));
+    my_function(Box::new(String::from("Abc")));
+    println!("Success");
 }
 
-fn draw_with_box(x: Box<dyn Draw>) {
-    x.draw();
-}
-fn draw_with_ref(y: &dyn Draw) {
-    y.draw();
+trait MyTrait {
+    fn f(&self) -> Box<dyn MyTrait>;
 }
 
-
-trait Draw {
-    fn draw(&self)-> String;
-}
-
-impl Draw for u8{
-    fn draw(&self) -> String {
-        format!("u8: {}", *self)
+impl MyTrait for u8 {
+    fn f(&self) -> Box<dyn MyTrait> {
+        Box::new(43)
     }
 }
 
-impl Draw for f64 {
-    fn draw(&self)-> String {
-        format!("f64: {}", *self)
+impl MyTrait for String {
+    fn f(&self) -> Box<dyn MyTrait> {
+        Box::new(self.clone())
     }
 }
 
+fn my_function(x: Box<dyn MyTrait>) -> Box<dyn MyTrait> {
+    x.f()
+}
 
+// trait Foo {
+// fn method(&self) ->String;
+// }
+
+// impl Foo for u8 {
+//     fn method(&self) -> String {
+//         format!("u8: {} ", *self)
+//     }
+// }
+
+// impl Foo for f64 {
+//     fn method(&self) -> String {
+//         format!("u64: {}", *self)
+//     }
+// }
+
+// fn static_dispatch<T:Foo>(x: T) {
+//    println!("{:#?}",x.method());
+// }
+
+// fn dynamic_dispatch(x: &dyn Foo) {
+//     println!("{:#?}",x.method());
+// }
+
+// fn draw_with_box(x: Box<dyn Draw>) {
+//     x.draw();
+// }
+// fn draw_with_ref(y: &dyn Draw) {
+//     y.draw();
+// }
+
+// trait Draw {
+//     fn draw(&self)-> String;
+// }
+
+// impl Draw for u8{
+//     fn draw(&self) -> String {
+//         format!("u8: {}", *self)
+//     }
+// }
+
+// impl Draw for f64 {
+//     fn draw(&self)-> String {
+//         format!("f64: {}", *self)
+//     }
+// }
 
 // // IMPLEMENT this function.
 // fn hatch_a_bird(species: u8) -> Box<dyn Bird> {
