7a278a024ffab6d80ae0b252f4d8062b95ae3f4f
muhammadjehanzaib <soulsniper155@gmail.com>
2025-01-31 08:17:21 +0500

    Exercise 1 Solved.

--- topics/hashmap/src/main.rs
+++ topics/hashmap/src/main.rs
@@ -1,3 +1,23 @@
+use std::collections::HashMap;
 fn main() {
-    
+    let mut scores = HashMap::new();
+    scores.insert("Sunface", 98);
+    scores.insert("Sunface1", 198);
+    scores.insert("Sunface2", 928);
+    scores.insert("Sunface3", 938);
+
+    println!("{:?}", scores);
+
+    let score = scores.get("Sunface");
+    assert_eq!(score, Some(&98)); // or
+    assert_eq!(score, Some(98).as_ref());
+
+    if scores.contains_key("Sunface1") {
+        let score = scores["Sunface1"];
+        assert_eq!(score, 198);
+        scores.remove("Sunface1");
+    }
+
+    assert_eq!(scores.len(), 3);
+    println!("{:?}", scores);
 }
