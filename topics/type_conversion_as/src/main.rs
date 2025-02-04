6d73210365451a438f4ef668c7516c2b08cdfefd
Soulsniper <soulsniper155@gmail.com>
2025-02-05 00:40:05 +0500

    Exercise Solved

--- topics/type_conversion_as/src/main.rs
+++ topics/type_conversion_as/src/main.rs
@@ -1,13 +1,22 @@
+// #![allow(overflowing_literals)]
 fn main() {
     // Exercise 1
-    let decimal = 97.123_f32;
+    // let decimal = 97.123_f32;
 
-    let integer: u8 = decimal as u8;
+    // let integer: u8 = decimal as u8;
 
-    let c1: char = decimal as u8 as char;
-    let c2 = integer as char;
+    // let c1: char = decimal as u8 as char;
+    // let c2 = integer as char;
 
-    assert_eq!(integer, 'a' as u8);
+    // assert_eq!(integer, 'a' as u8);
 
-    println!("Success!");
+    // println!("Success!");
+
+    // Exercise 2
+    // by default overflow will cause compile errors, but we can add global annotation to suppress these errors.
+    // assert_eq!(u8::MAX, 255);
+
+    // let v = 1000 as u8;
+    // println!("Success");
+
 }
