30241fa0bc165a4b5dc34377dea3647a8a2b99c0
muhammadjehanzaib <soulsniper155@gmail.com>
2025-02-04 21:42:32 +0500

    Exercise 1 Solved

--- topics/type_conversion_as/src/main.rs
+++ topics/type_conversion_as/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+
+    let decimal = 97.123_f32;
+
+    let integer: u8 = decimal as u8;
+
+    let c1: char = decimal as u8 as char;
+    let c2 = integer as char;
+
+    assert_eq!(integer, 'a' as u8);
+
+    println!("Success!");
 }
